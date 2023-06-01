08aa2f89ca55322f701532200ace1d1e1668a3a7
nemizar <nemizar@gmail.com>
2023-06-01 20:28:24 +0300

    HW1

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	hello := "Hello, OTUS!"
+
+	reversedString := stringutil.Reverse(hello)
+
+	fmt.Println(reversedString)
 }
