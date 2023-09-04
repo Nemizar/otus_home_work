8c2993711685fcbf5c4b52772a34608d89f11483
nemizar <nemizar@gmail.com>
2023-09-04 19:14:17 +0300

    HW7

--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -21,7 +21,7 @@ func init() {
 func main() {
 	flag.Parse()
 
-	if err := Copy(from, to, limit, offset); err != nil {
+	if err := Copy(from, to, offset, limit); err != nil {
 		fmt.Println(err.Error())
 
 		os.Exit(-1)
